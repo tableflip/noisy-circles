80e226849709020401e8fa06dc6813a84cf9258f
Liam Wooding <liamwooding@gmail.com>
2014-05-16 16:43:41 +0100

    Circle radius is now tied to volume

--- js/pitchdetect.js
+++ js/pitchdetect.js
@@ -27,7 +27,7 @@ var sourceNode = null;
 var analyser = null;
 var theBuffer = null;
 var detectorElem,
-    flashElem;
+  flashElem;
 var WIDTH = 300;
 var CENTER = 150;
 var HEIGHT = 42;
@@ -48,6 +48,7 @@ var colors = [
   "#007CBB",
   "#72FCFF"
 ]
+var volume = -35
 
 window.onload = function() {
   var request = new XMLHttpRequest();
@@ -66,120 +67,83 @@ window.onload = function() {
   //Initializing the canvas
   var canvas = document.getElementById("canvas");
 
-  setInterval(function () {
+  setInterval(function() {
     draw(canvas)
   }, 33)
 
   toggleLiveInput()
 }
 
-function error() {
-  alert('Stream generation failed.');
-}
-
-function getUserMedia(dictionary, callback) {
-  try {
-    navigator.getUserMedia =
-      navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia;
-    navigator.getUserMedia(dictionary, callback, error);
-  } catch (e) {
-    alert('getUserMedia threw exception :' + e);
-  }
-}
-
-function gotStream(stream) {
-  // Create an AudioNode from the stream.
-  var mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-  // Connect it to the destination.
-  analyser = audioContext.createAnalyser();
-  analyser.fftSize = 2048;
-  mediaStreamSource.connect(analyser);
-  updatePitch();
-}
-
-function toggleLiveInput() {
-  getUserMedia({
-    audio: true
-  }, gotStream);
-}
-
-function togglePlayback() {
-  var now = audioContext.currentTime;
-
-  if (isPlaying) {
-    //stop playing and return
-    sourceNode.stop(now);
-    sourceNode = null;
-    analyser = null;
-    isPlaying = false;
-    if (!window.cancelAnimationFrame)
-      window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-    window.cancelAnimationFrame(rafID);
-    return "start";
+  function error() {
+    alert('Stream generation failed.');
   }
 
-  sourceNode = audioContext.createBufferSource();
-  sourceNode.buffer = theBuffer;
-  sourceNode.loop = true;
+  function getUserMedia(dictionary, callback) {
+    try {
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+      navigator.getUserMedia(dictionary, callback, error);
+    } catch (e) {
+      alert('getUserMedia threw exception :' + e);
+    }
+  }
 
-  analyser = audioContext.createAnalyser();
-  analyser.fftSize = 2048;
-  sourceNode.connect(analyser);
-  analyser.connect(audioContext.destination);
-  sourceNode.start(now);
-  isPlaying = true;
-  isLiveInput = false;
-  updatePitch();
+  function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
-  return "stop";
-}
+    // Connect it to the destination.
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 64;
+    mediaStreamSource.connect(analyser);
+    updatePitch();
+
+    // processor.onaudioprocess = function(e) {
+    //   var out = e.outputBuffer.getChannelData(0);
+    //   var int = e.inputBuffer.getChannelData(0);
+    //   var max = 0;
+
+    //   for (var i = 0; i < int.length; i++) {
+    //     out[i] = int[i]; //prevent feedback and we only need the input data
+    //     max = int[i] > max ? int[i] : max;
+    //   }
+    //   //convert from magitude to decibel
+    //   var db = 20 * Math.log(Math.max(max, Math.pow(10, -72 / 20))) / Math.LN10;
+    //   //It's time to draw on the canvas
+    //   //create the gradient
+    //   console.log(db)
+
+    //   // var grad = ctx.createLinearGradient(w / 10, h * 0.2, w / 10, h * 0.95);
+    //   // grad.addColorStop(0, 'red');
+    //   // grad.addColorStop(-6 / -72, 'yellow');
+    //   // grad.addColorStop(1, 'green');
+    //   // //fill the background
+    //   // ctx.fillStyle = '#555';
+    //   // ctx.fillRect(0, 0, w, h);
+    //   // ctx.fillStyle = grad;
+    //   // //draw the rectangle
+    //   // ctx.fillRect(w / 10, h * 0.8 * (db / -72), w * 8 / 10, (h * 0.95) - h * 0.8 * (db / -72));
+    //   // //draw the text out
+    //   // ctx.fillStyle = "white";
+    //   // ctx.font = "Arial 12pt";
+    //   // ctx.textAlign = "center";
+    //   // ctx.fillText(Math.round(db * 100) / 100 + ' dB', w / 2, h - h * 0.025);
+    // };
+  }
+
+  function toggleLiveInput() {
+    getUserMedia({
+      audio: true
+    }, gotStream);
+  }
 
 var rafID = null;
 var tracks = null;
 var buflen = 2048;
 var buf = new Uint8Array(buflen);
 var MINVAL = 134; // 128 == zero.  MINVAL is the "minimum detected signal" level.
-
-/*
-function findNextPositiveZeroCrossing( start ) {
-  var i = Math.ceil( start );
-  var last_zero = -1;
-  // advance until we're zero or negative
-  while (i<buflen && (buf[i] > 128 ) )
-    i++;
-  if (i>=buflen)
-    return -1;
-
-  // advance until we're above MINVAL, keeping track of last zero.
-  while (i<buflen && ((t=buf[i]) < MINVAL )) {
-    if (t >= 128) {
-      if (last_zero == -1)
-        last_zero = i;
-    } else
-      last_zero = -1;
-    i++;
-  }
-
-  // we may have jumped over MINVAL in one sample.
-  if (last_zero == -1)
-    last_zero = i;
-
-  if (i==buflen)  // We didn't find any more positive zero crossings
-    return -1;
-
-  // The first sample might be a zero.  If so, return it.
-  if (last_zero == 0)
-    return 0;
-
-  // Otherwise, the zero might be between two values, so we need to scale it.
-
-  var t = ( 128 - buf[last_zero-1] ) / (buf[last_zero] - buf[last_zero-1]);
-  return last_zero+t;
-}
-*/
 
 var noteStrings = ["c", "csharp", "d", "dsharp", "e", "f", "fsharp", "g", "gsharp", "a", "asharp", "b"];
 
@@ -195,42 +159,6 @@ function frequencyFromNoteNumber(note) {
 function centsOffFromPitch(frequency, note) {
   return (1200 * Math.log(frequency / frequencyFromNoteNumber(note)) / Math.log(2));
 }
-
-// this is a float version of the algorithm below - but it's not currently used.
-/*
-function autoCorrelateFloat( buf, sampleRate ) {
-  var MIN_SAMPLES = 4;  // corresponds to an 11kHz signal
-  var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
-  var SIZE = 1000;
-  var best_offset = -1;
-  var best_correlation = 0;
-  var rms = 0;
-
-  if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
-    return -1;  // Not enough data
-
-  for (var i=0;i<SIZE;i++)
-    rms += buf[i]*buf[i];
-  rms = Math.sqrt(rms/SIZE);
-
-  for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
-    var correlation = 0;
-
-    for (var i=0; i<SIZE; i++) {
-      correlation += Math.abs(buf[i]-buf[i+offset]);
-    }
-    correlation = 1 - (correlation/SIZE);
-    if (correlation > best_correlation) {
-      best_correlation = correlation;
-      best_offset = offset;
-    }
-  }
-  if ((rms>0.1)&&(best_correlation > 0.1)) {
-    console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")");
-  }
-//  var best_frequency = sampleRate/best_offset;
-}
-*/
 
 function autoCorrelate(buf, sampleRate) {
   var MIN_SAMPLES = 4; // corresponds to an 11kHz signal
@@ -267,74 +195,28 @@ function autoCorrelate(buf, sampleRate) {
   if ((rms > 0.01) && (best_correlation > 0.01)) {
     confidence = best_correlation * rms * 10000;
     currentPitch = sampleRate / best_offset;
-    // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
   }
-  //  var best_frequency = sampleRate/best_offset;
 }
 
 function updatePitch(time) {
-  var cycles = new Array;
-  analyser.getByteTimeDomainData(buf);
+  var array = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(array);
 
-  /*
-// old zero-crossing code
-
-  var i=0;
-  // find the first point
-  var last_zero = findNextPositiveZeroCrossing( 0 );
-
-  var n=0;
-  // keep finding points, adding cycle lengths to array
-  while ( last_zero != -1) {
-    var next_zero = findNextPositiveZeroCrossing( last_zero + 1 );
-    if (next_zero > -1)
-      cycles.push( next_zero - last_zero );
-    last_zero = next_zero;
-
-    n++;
-    if (n>1000)
-      break;
-  }
-
-  // 1?: average the array
-  var num_cycles = cycles.length;
-  var sum = 0;
-  var pitch = 0;
-
-  for (var i=0; i<num_cycles; i++) {
-    sum += cycles[i];
-  }
-
-  if (num_cycles) {
-    sum /= num_cycles;
-    pitch = audioContext.sampleRate/sum;
-  }
-
-// confidence = num_cycles / num_possible_cycles = num_cycles / (audioContext.sampleRate/)
-  var confidence = (num_cycles ? ((num_cycles/(pitch * buflen / audioContext.sampleRate)) * 100) : 0);
-  */
-
-  /*
-  console.log( 
-    "Cycles: " + num_cycles + 
-    " - average length: " + sum + 
-    " - pitch: " + pitch + "Hz " +
-    " - note: " + noteFromPitch( pitch ) +
-    " - confidence: " + confidence + "% "
-    );
-*/
+  volume = getAverageVolume(array)
+  console.log('VOLUME:' + volume); //here's the volume
   // possible other approach to confidence: sort the array, take the median; go through the array and compute the average deviation
+  analyser.getByteTimeDomainData(buf);
   autoCorrelate(buf, audioContext.sampleRate);
 
   //  detectorElem.className = (confidence>50)?"confident":"vague";
 
-  if (confidence < 10) {
+  if (confidence < 90) {
     //detectorElem.className = "vague";
   } else {
     //flash_screen(canvas)
     var note = noteFromPitch(currentPitch);
     //for (i = 0; i < 3; i++) {
-      particles.push(new create_particle(canvas, note % 12));
+    particles.push(new create_particle(canvas, note % 12));
     //}
     var detune = centsOffFromPitch(currentPitch, note);
     if (detune == 0) {
@@ -361,55 +243,70 @@ function updatePitch(time) {
 
 //Lets animate the particle
 
-  function draw (canvas) {
-    var ctx = canvas.getContext("2d");
-    var W = canvas.width
-    var H = canvas.height
-    //Lets reduce the opacity of the BG paint to give the final touch
-    ctx.fillStyle = "#25afee";
-    ctx.fillRect(0, 0, W, H);
+function draw(canvas) {
+  var ctx = canvas.getContext("2d");
+  var W = canvas.width
+  var H = canvas.height
+  //Lets reduce the opacity of the BG paint to give the final touch
+  ctx.fillStyle = "#25afee";
+  ctx.fillRect(0, 0, W, H);
 
-    //Lets blend the particle with the BG
-    ctx.globalCompositeOperation = "source-over";
+  //Lets blend the particle with the BG
+  ctx.globalCompositeOperation = "source-over";
 
-    //Lets draw particles from the array now
-    for (var t = 0; t < particles.length; t++) {
-      var p = particles[t];
+  //Lets draw particles from the array now
+  for (var t = 0; t < particles.length; t++) {
+    var p = particles[t];
 
-      ctx.beginPath();
+    ctx.beginPath();
 
-      ctx.arc(p.x, p.y, 12, Math.PI * 2, false);
-      ctx.fillStyle = p.color;
-      ctx.fill();
+    ctx.arc(p.x, p.y, volume / 10, Math.PI * 2, false);
+    ctx.fillStyle = p.color;
+    ctx.fill();
 
-      p.x += p.vx;
-      p.y += p.vy;
+    p.x += p.vx;
+    p.y += p.vy;
 
-      //To delete the balls once they've moved out of the canvas
-      if (p.x < -50 || p.y < -50 || p.x > W + 50 || p.y > H + 50) particles.splice(t, 1)
-    }
+    // delete particles once they've moved out of the canvas
+    if (p.x < -50 || p.y < -50 || p.x > W + 50 || p.y > H + 50) particles.splice(t, 1)
+  }
+}
+
+// Creates a particle
+
+function create_particle(canvas, radius) {
+  var W = canvas.width
+  var H = canvas.height
+  // Center on the canvas
+  this.x = W / 2;
+  this.y = H / 2;
+  // Let's add random velocity to each particle
+  this.vx = Math.random() * 40 - 20;
+  this.vy = Math.random() * 40 - 20;
+  this.color = colors[radius];
+
+  //Random size
+  this.radius = radius;
+}
+
+function flash_screen(canvas) {
+  flashElem.className = "white"
+  setTimeout(function() {
+    flashElem.className = ""
+  }, 10)
+}
+
+function getAverageVolume(array) {
+  var values = 0;
+  var average;
+
+  var length = array.length;
+
+  // get all the frequency amplitudes
+  for (var i = 0; i < length; i++) {
+    values += array[i];
   }
 
-  //Lets create a function which will help us to create multiple particles
-  function create_particle (canvas, radius) {
-    var W = canvas.width
-    var H = canvas.height
-    //Random position on the canvas
-    this.x = W / 2;
-    this.y = H / 2;
-
-    //Lets add random velocity to each particle
-    this.vx = Math.random() * 40 - 20;
-    this.vy = Math.random() * 40 - 20;
-    this.color = colors[radius];
-
-    //Random size
-    this.radius = radius;
-  }
-
-  function flash_screen (canvas) {
-    flashElem.className = "white"
-    setTimeout(function () {
-      flashElem.className = ""
-    }, 10)
-  }
+  average = values / length;
+  return average;
+}
